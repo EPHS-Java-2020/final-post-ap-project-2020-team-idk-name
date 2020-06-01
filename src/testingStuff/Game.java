692f1b4566f211ddfbcb82d74346bf63e3e63a57
Rishi <61213336+rishi-n@users.noreply.github.com>
2020-06-01 10:09:40 -0500

    strt and end

--- src/testingStuff/Game.java
+++ src/testingStuff/Game.java
@@ -10,40 +10,47 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-
-
 public class Game implements KeyListener, ActionListener {
 
-	public static final int FPS = 60, WIDTH = 1366, HEIGHT = 695,floorHeight=500;
-	
+	public static final int FPS = 60, WIDTH = 1366, HEIGHT = 695, floorHeight = 500;
 
 	private TheDude dude;
 	private JFrame frame;
 	private JPanel panel;
 	private Timer t;
 	private int met;
-	private ArrayList<Meteor> m=new ArrayList<Meteor>();
-	private Random r=new Random();
-	private int interval=60;
-	private ArrayList<Drink> d=new ArrayList<Drink>();	
-	private int driInt=900;
-	private int drink=0;
-	private int drinkLenMax=300;
+	private ArrayList<Meteor> m = new ArrayList<Meteor>();
+	private Random r = new Random();
+	private int interval = 60;
+	private ArrayList<Drink> d = new ArrayList<Drink>();
+	private int driInt = 900;
+	private int drink = 0;
+	private int drinkLenMax = 300;
 	private int drinkLen;
-	private ArrayList<Fireball> f= new ArrayList<Fireball>();
+	private ArrayList<Fireball> f = new ArrayList<Fireball>();
 	private int fireInt = 600;
 	private int fire = 0;
-	private int lives=3;
-	private int score=0;
-	
+	private int lives = 3;
+	private int score = 0;
+	private boolean isPlaying = false;
+	private boolean isDead = false;
+
 	public static void main(String[] args) {
 		new Game().go();
+	}
+	
+	public boolean getPlaying() {
+		return isPlaying;
+	}
+	
+	public boolean getDead() {
+		return isDead;
 	}
 
 	public void go() {
 		frame = new JFrame("");
 		dude = new TheDude();
-		panel = new GamePanels(this, dude, m, d,f);
+		panel = new GamePanels(this, dude, m, d, f);
 		frame.add(panel);
 
 		frame.setSize(WIDTH, HEIGHT);
@@ -69,90 +76,103 @@ public class Game implements KeyListener, ActionListener {
 		if (e.getKeyCode() == KeyEvent.VK_DOWN) {
 			dude.crouch();
 		}
+		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+			isPlaying = true;
+			isDead=false;
+		}
 	}
-	
+
 	public int getLives() {
 		return lives;
 	}
 	
+	public void setLives(int num) {
+		lives=num;
+	}
+
 	public int getScore() {
 		return score;
 	}
 
 	public void actionPerformed(ActionEvent e) {
 		panel.repaint();
-		dude.physics();
-		if (met==interval) {
-			m.add(new Meteor());
-			interval=(r.nextInt(6)+1)*15;
-			
-			met=0;
-		}
-		if(fire == fireInt) {
-			f.add(new Fireball());
-			fireInt = (r.nextInt(11)+10)*60;
-			fire = 0;
-		}
-		if(drink==driInt) {
-			d.add(new Drink(dude.getRect()));
-			driInt= (r.nextInt(11)+10)*60;
-			drink=0;
-		}
-		ArrayList<Meteor> toRemove=new ArrayList<Meteor>();
-		for (Meteor n:m) {
-			n.physics();
-			if (dude.getRect().intersects(n.getRect())) {
-				toRemove.add(n);
-				n.y=1000;
-				lives--;
-			}
-			if(n.y>=1000) {
-				toRemove.add(n);
-			}
-		}
-		ArrayList<Fireball> toRemoveF= new ArrayList<Fireball>();
-		for (Fireball z:f) {
-			z.physics();
-			if(dude.getRect().intersects(z.getRect())) {
-				toRemoveF.add(z);
-				lives--;
-			}
-			if(z.x>=1500) {
-				toRemoveF.add(z);
-			}
-			
-		}
-		for (Meteor r:toRemove) {
-			m.remove(r);
-		}
-		for (Fireball fir: toRemoveF) {
-			f.remove(fir);
-		}
-		for(Drink r:d) {
-			if (dude.getRect().intersects(r.getRect())) {
-				r.remove();
-				dude.setSlow(true);
-				drinkLen=0;
-			}
-		}
-		if(drinkLen==drinkLenMax) {
-			dude.setSlow(false);
-		}
-		if(dude.isSlow()) {
-			drinkLen++;
-		}
-		met++;
-		drink++;
-		fire++;
-		score++;
-	}
+		if (isPlaying) {
+			dude.physics();
+			if (met == interval) {
+				m.add(new Meteor());
+				interval = (r.nextInt(6) + 1) * 15;
 
+				met = 0;
+			}
+			if (fire == fireInt) {
+				f.add(new Fireball());
+				fireInt = (r.nextInt(11) + 10) * 60;
+				fire = 0;
+			}
+			if (drink == driInt) {
+				d.add(new Drink(dude.getRect()));
+				driInt = (r.nextInt(11) + 10) * 60;
+				drink = 0;
+			}
+			ArrayList<Meteor> toRemove = new ArrayList<Meteor>();
+			for (Meteor n : m) {
+				n.physics();
+				if (dude.getRect().intersects(n.getRect())) {
+					toRemove.add(n);
+					n.y = 1000;
+					lives--;
+				}
+				if (n.y >= 1000) {
+					toRemove.add(n);
+				}
+			}
+			ArrayList<Fireball> toRemoveF = new ArrayList<Fireball>();
+			for (Fireball z : f) {
+				z.physics();
+				if (dude.getRect().intersects(z.getRect())) {
+					toRemoveF.add(z);
+					lives--;
+				}
+				if (z.x >= 1500) {
+					toRemoveF.add(z);
+				}
+
+			}
+			for (Meteor r : toRemove) {
+				m.remove(r);
+			}
+			for (Fireball fir : toRemoveF) {
+				f.remove(fir);
+			}
+			for (Drink r : d) {
+				if (dude.getRect().intersects(r.getRect())) {
+					r.remove();
+					dude.setSlow(true);
+					drinkLen = 0;
+				}
+			}
+			if (drinkLen == drinkLenMax) {
+				dude.setSlow(false);
+			}
+			if (dude.isSlow()) {
+				drinkLen++;
+			}
+			met++;
+			drink++;
+			fire++;
+			score++;
+			if(lives==0) {
+				isDead=true;
+				isPlaying=false;
+			}
+		}
+		
+	}
 
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 
 	}
-
 
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
--- src/testingStuff/GamePanels.java
+++ src/testingStuff/GamePanels.java
@@ -12,30 +12,28 @@ import javax.swing.JPanel;
 
 public class GamePanels extends JPanel {
 
-
 	private static final long serialVersionUID = 1L;
 	private TheDude dude;
 	private Game game;
 	private Image background;
 	private Image heart;
 	private ArrayList<Meteor> m = new ArrayList<>();
-	private ArrayList<Drink> d=new ArrayList<>();
+	private ArrayList<Drink> d = new ArrayList<>();
 	private ArrayList<Fireball> f = new ArrayList<>();
 
-	public GamePanels(Game game, TheDude dude, ArrayList<Meteor> m, ArrayList<Drink> d,ArrayList<Fireball>f) {
+	public GamePanels(Game game, TheDude dude, ArrayList<Meteor> m, ArrayList<Drink> d, ArrayList<Fireball> f) {
 		this.dude = dude;
 		this.game = game;
 		this.m = m;
-		this.d=d;
-		this.f=f;
-//		this.h=h;
+		this.d = d;
+		this.f = f;
 		try {
 			background = ImageIO.read(new File("background.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		try {
-			heart=ImageIO.read(new File("Heart-Icon.png"));
+			heart = ImageIO.read(new File("Heart-Icon.png"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -46,23 +44,37 @@ public class GamePanels extends JPanel {
 	public void paintComponent(Graphics g1) {
 		g1.fillRect(0, 0, Game.WIDTH, Game.HEIGHT);
 		g1.drawImage(background, 0, 0, Game.WIDTH, Game.HEIGHT, null);
-		g1.setColor(new Color(56,24,0));
+		g1.setColor(new Color(56, 24, 0));
 		g1.fillRect(0, Game.floorHeight, Game.WIDTH, Game.HEIGHT - Game.floorHeight);
-		dude.update(g1);
-		for(Drink r:d) {
-			r.update(g1);
+		if (game.getDead()) {
+			g1.setFont(new Font("Super Mario 256", Font.PLAIN, 40));
+			g1.setColor(Color.black);
+			g1.drawString("You died. Your score was "+game.getScore(), 400, 200);
+			g1.drawString("Press space to play again", 400, 250);
+			game.setLives(3);
+		} else if (game.getPlaying()) {
+			dude.update(g1);
+			for (Drink r : d) {
+				r.update(g1);
+			}
+			for (Meteor e : m) {
+				e.update(g1);
+			}
+			for (Fireball x : f) {
+				x.update(g1);
+			}
+			g1.setFont(new Font("Super Mario 256", Font.PLAIN, 40));
+			g1.setColor(Color.black);
+			g1.drawString("Score: " + game.getScore(), 30, 50);
+			g1.drawImage(heart, 1250, 20, 30, 30, null);
+			g1.drawString("x" + game.getLives(), 1280, 50);
+		} else {
+			g1.setFont(new Font("Super Mario 256", Font.PLAIN, 40));
+			g1.setColor(Color.black);
+			g1.drawString("WELCOME TO GAME NAME", 400, 200);
+			g1.drawString("PRESS SPACE TO START", 400, 250);
+
 		}
-		for (Meteor e : m) {
-			e.update(g1);
-		}
-		for (Fireball x:f) {
-			x.update(g1);
-		}
-		g1.setFont(new Font("Super Mario 256", Font.PLAIN, 40));
-		g1.setColor(Color.black);
-		g1.drawString("Score: "+game.getScore(), 30, 50);
-		g1.drawImage(heart,1250, 20, 30,30,null);
-		g1.drawString("x"+game.getLives(), 1280, 50);
 	}
 
 }
